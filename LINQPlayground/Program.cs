39961330ca1b8f677187142d417a0b8e9f65fa2a
Petr Barabáš <pbarabas@outlook.cz>
2025-02-14 16:12:11 +0100

    added playground

---
+++ LINQPlayground/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQPlayground
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
